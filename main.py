ef95d76e3e5edfac8c7d159e18eb73c8e20e76df
markus <marco.prinzi@edu-its.it>
2026-07-14 15:23:33 +0200

    inizio calcolatrice di rete

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import abc
 import re
 from typing import Dict, Any, Tuple, List
-
+"classe base astratta"
 class NetworkObject(abc.ABC):
     CIDR_PATTERN = re.compile(r"^([0-9a-fA-F\.\:]+)/(\d+)$")
 
 def __init__(self, raw_input: str):
     self.raw_input = raw_input.strip()
     self.ip_str, self.prefix_len = self._validate_and_parse()
+
 
